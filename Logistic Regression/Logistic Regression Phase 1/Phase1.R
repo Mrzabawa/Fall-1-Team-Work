b54b85920edce404436b4216798915cdf8de3cd8
Michael Zabawa <mrzabawa@ncsu.edu>
2019-08-29 14:29:32 -0400

    Notes in phase 1

--- Logistic Regression/Logistic Regression Phase 1/Phase1.R
+++ Logistic Regression/Logistic Regression Phase 1/Phase1.R
@@ -4,7 +4,7 @@
 #                Team Blue 5                                                  #
 ###############################################################################
 library('haven')#for read_sas()
-
+#load data
 dataTrain <- read_sas('insurance_t.sas7bdat')
 #modifies BRANCH and RES to factors for analysis
 dataTrain$BRANCH <- as.factor(dataTrain$BRANCH)
@@ -13,5 +13,30 @@ dataTrain$RES <- as.factor(dataTrain$RES)
 summary(dataTrain)
 #binary? DDA, DIRDEP, NSF, SAV, ATM, CD, IRA, LOC, INV, ILS, MM, MTG, CC, SDB, HMOWN, MOVED, INAREA, (INS <- Target)
 #NA's ACCTAGE, PHONE, POS, POSAMT, INV, INVBAL, CC, CCBAL, CCPURC, INCOME, HMOWN, LORES, HMVAL, AGE, CRSCORE
+
+# Bullet point 1:
+# Verify alpha
+# Run simple logistic/test of association on all variables - pulling out the significant ones
+
+# Bullet point 2:
+# Find the binary variables
+# Run the odds
+# Create a table
+# Talk about the largest one (include any additional interesting findings)
+
+# Bullet point 3:
+# Test the linearity assumptions for continuous variables (logit stuff?)
+# Qq plots 
+# Y = log odds (logits)
+# X = predictor variable (continuous)
+
+# Bullet point 4:
+# Visual representation of missing values
+# Examine multicollinearity 
+# Write the report 
+
+
+
+
 
 
