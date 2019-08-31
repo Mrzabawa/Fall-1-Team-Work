895de9124758f8cfacbd97c28a8ca55e4e499fc6
Michael Zabawa <mrzabawa@ncsu.edu>
2019-08-31 09:00:35 -0400

    commit to pull

--- Logistic Regression/Logistic Regression Phase 1/Phase1.R
+++ Logistic Regression/Logistic Regression Phase 1/Phase1.R
@@ -6,11 +6,14 @@
 library('haven')#for read_sas()
 #load data
 dataTrain <- read_sas('insurance_t.sas7bdat')
+setwd('/Users/michaelzabawa/Documents/GitHub/Fall\ 1\ Team\ Work/Logistic\ Regression/Data')
 #modifies BRANCH and RES to factors for analysis
 dataTrain$BRANCH <- as.factor(dataTrain$BRANCH)
 dataTrain$RES <- as.factor(dataTrain$RES)
 #Looks at the 5 number summary of the data
 summary(dataTrain)
+length(levels(dataTrain$BRANCH))
+head(dataTrain$ACCTAGE, n= 10)
 #binary? DDA, DIRDEP, NSF, SAV, ATM, CD, IRA, LOC, INV, ILS, MM, MTG, CC, SDB, HMOWN, MOVED, INAREA, (INS <- Target)
 #NA's ACCTAGE, PHONE, POS, POSAMT, INV, INVBAL, CC, CCBAL, CCPURC, INCOME, HMOWN, LORES, HMVAL, AGE, CRSCORE
 
