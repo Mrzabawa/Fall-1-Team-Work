af6d1013673ea85aaf04893d3033f77511326316
bjsulli3 <54413894+bjsulli3@users.noreply.github.com>
2019-09-16 14:44:00 -0400

    Update Continous_assumptions.R

--- Logistic Regression/Logistic Regression Phase 1/Continous_assumptions.R
+++ Logistic Regression/Logistic Regression Phase 1/Continous_assumptions.R
@@ -5,7 +5,7 @@ install.packages("brglm")
 install.packages("car")
 install.packages("mgcv")
 install.packages("haven")
-install.packages("car")
+
 
 library(MASS)
 library(visreg)
@@ -13,7 +13,7 @@ library(brglm)
 library(car)
 library(mgcv)
 library(haven)
-library(car)
+
 
 path <- system.file('insurance_t.sas7bdat')
 
@@ -29,4 +29,16 @@ fit.gam <- gam(INS ~ s(ACCTAGE) + s(DEPAMT) + s(CHECKS) +
                       s(TELLER) + s(SAVBAL) + s(ATMAMT) + s(POS) + s(POSAMT) + s(CDBAL) + s(IRABAL) +
                       s(INVBAL) + s(ILSBAL) + s(MMBAL) + s(MTGBAL) + s(INCOME) +
                       s(LORES) + s(HMVAL) + s(AGE) + s(CRSCORE) , data = data, family = binomial(link = 'logit'), method = 'REML')
+gamsum <- summary(fit.gam)
 summary(fit.gam)
+
+gamacct <- gam(INS ~ s(ACCTAGE), data = data, family = binomial(link = 'logit'), method = 'REML')
+summary(gamacct)
+
+models = sapply(setdiff(names(data),"INS"),
+                function(x) summary(gam(reformulate(x,response="INS"),
+                data=data,family=binomial(link = 'logit'), method = 'REML')))
+
+
+write.csv(gamsum, "C:/Users/bjsul/Desktop/continuous_assumptions.csv")
+
